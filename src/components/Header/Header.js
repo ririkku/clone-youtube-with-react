7fbff4500b18755780377a59528ff6fb2ea17a88
ririkku <ryoji19950109@gmail.com>
2020-10-20 21:42:50 +0900

    検索の準備

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, {useState, useEffect, useContext} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import {Link} from 'react-router-dom'
 import Style from './Header.module.scss'
+import {useHistory} from 'react-router-dom'
+import {Store} from '../../stores/index'
 
 const Header = () => {
+
+  const {globalState, setGlobalState} = useContext(Store)
+
+  const [term, setTerm] = useState('')
+
+  const history = useHistory()
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setGlobalState({type: 'SET_TERM', payload: {term}})
+    history.push(`/search?query=${term}`)
+  }
+
+  useEffect(() => {
+    setTerm(globalState.term)
+  }, [])
+
   return (
     <div className={Style.header}>
       <div className={Style.item}>
         <Link to="/">VideoTube</Link>
       </div>
       <div className={Style.item}>
-        <form>
-          <input type="text" placeholder="search word"></input>
+        <form onSubmit={handleSubmit} >
+          <input 
+            type="text" 
+            value={term} 
+            placeholder="search word"
+            onChange={e => setTerm(e.target.value)}></input>
           <button type="submit"><FontAwesomeIcon icon={faSearch} /></button>
         </form>
       </div>
